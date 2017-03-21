deac428b565173a7a792df5ed574598b8e79dfcb
NikitaMishin <Nikitamishin97@gmail.com>
2017-03-21 20:10:32 +0300

    Update hello.py

--- web/hello.py
+++ web/hello.py
@@ -1,1 +1,9 @@
 
+
+def wsgi_app(environ,start_response):
+    status = "200 OK"
+    headers = ["Content-Type", "text/plain"]
+    body = [environ["QueryString"].replace("&","\r\n")]
+	start_response(status,headers)
+	return [body]
+    
