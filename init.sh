645c510666a76be0c5ce459b1c1f2e84bf7be4b6
NikitaMishin <Nikitamishin97@gmail.com>
2017-03-21 23:01:44 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app  &
+
+sudo gunicorn -b 0.0.0.0:8080 hello:app &
