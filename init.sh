11aabd57016e3ab265fa9938f936a81424365687
NikitaMishin <Nikitamishin97@gmail.com>
2017-03-24 00:16:43 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 # sudo /etc/init.d/nginx restart
 # sudo ln -sf /home/box/web/etc/guniconf.py   /etc/gunicorn.d/guniconf
 # sudo /etc/init.d/gunicorn restart
-# sudo gunicorn -c /etc/gunicorn.d/guniconf wsgi:application 
+# sudo gunicorn -c /etc/gunicorn.d/guniconf wsgi:application
 sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
