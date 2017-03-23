0d90b5eb4bed2ab44363565f5fa9a2a003c4077b
NikitaMishin <Nikitamishin97@gmail.com>
2017-03-24 00:13:34 +0300

    Update guniconf.py

--- web/etc/guniconf.py
+++ web/etc/guniconf.py
@@ -1,2 +1,12 @@
-bind = "0.0.0.0:8000"
-pythonpath="/home/box/web/ask/"
+CONFIG = {
+    'mode': 'django',
+    'working_dir': '/home/box/web/ask',
+    #'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8000',
+        #'--workers=16',
+        #'--timeout=60',
+		'--log-level=debug',
+        'ask',
+    ),
+}
