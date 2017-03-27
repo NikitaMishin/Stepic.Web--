fd64caa1110c0df6a1f14375babcd486148c14da
NikitaMishin <Nikitamishin97@gmail.com>
2017-03-27 14:43:52 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -14,18 +14,10 @@ sudo /etc/init.d/nginx restart
 # sudo /etc/init.d/gunicorn restart
 
 
-
-sudo pip3 install --upgrade django
-sudo pip3 install --upgrade gunicorn
-sudo nano /usr/sbin/gunicorn-debian
-sudo nano /usr/bin/gunicorn
-sudo nano /usr/bin/gunicorn_django
-sudo nano /usr/bin/gunicorn_paster
-
 sudo ln -sf /home/box/web/etc/guniconf_hello.py   /etc/gunicorn.d/guniconf_hello
 sudo ln -sf /home/box/web/etc/guniconf_django.py   /etc/gunicorn.d/guniconf_django
 sudo /etc/init.d/gunicorn restart
 
 sudo gunicorn -c /etc/gunicorn.d/guniconf_hello hello:app
-cd ask/
+
 sudo gunicorn -c /etc/gunicorn.d/guniconf_django ask.wsgi:application
