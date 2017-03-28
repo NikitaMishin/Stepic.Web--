b88730dde739acde54b3f80997d5ec54a2eabe24
NikitaMishin <Nikitamishin97@gmail.com>
2017-03-28 23:19:05 +0300

    Update models.py

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -11,7 +11,7 @@ class Question(models.Model):
     title = models.CharField(max_length=200,unique = True,verbose_name="Title")
     text = models.TextField(verbose_name="Text")
     added_at = models.DateTimeField(auto_now_add=True,verbose_name="time_of_adding")
-    raiting = models.IntegerField(verbose_name = "raiting",default = 0)
+    rating = models.IntegerField(verbose_name = "raiting",default = 0)
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User,related_name ='question_like_user')
     
